509f2644f40d7ab930e84d1b32549c20148b963c
waqas hussain <waqashussainmvc@gmail.com>
2021-12-15 18:47:02 +0500

    home controller

--- ShoppingStope/Controllers/HomeController.cs
+++ ShoppingStope/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace ShoppingStope.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        { 
             
             return View();
         }
