f9dee47b1ac4bfd9e085a6cd3ee433a1e6834f75
waqas hussain <waqashussainmvc@gmail.com>
2021-12-15 18:44:31 +0500

    templete change

--- ShoppingStope/Controllers/HomeController.cs
+++ ShoppingStope/Controllers/HomeController.cs
@@ -10,8 +10,7 @@ namespace ShoppingStope.Controllers
     {
         public ActionResult Index()
         {
-            string saq = "hello how are you";
-            string xz = string.Empty;
+            
             return View();
         }
 
